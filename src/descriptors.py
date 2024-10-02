95dd97349f905ad7f2153290d2d570b4b5641691
Laura Haro Escoi <lauraha.escoi@gmail.com>
2024-10-02 22:17:16 +0200

    Add main.py and fixed bin format

--- src/descriptors.py
+++ src/descriptors.py
@@ -1,33 +1,40 @@
 # descriptors.py - Task 1
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 class ImageDescriptor:
 
-    def __init__(self, color_space, range, histogram_bins=255):
+    def __init__(self, color_space, histogram_bins=[8, 8, 8]):  # default value for histogram_bins [R, G, B]
         self.color_space = color_space
         self.histogram_bins = histogram_bins
-        self.range = range
+
 
     def describe(self, image):
         # select color space
         if self.color_space == 'CIELAB':
             image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
-            self.range = [0, 256, 0, 256, 0, 256]
+            range = [0, 256, 0, 256, 0, 256]
         elif self.color_space == 'HSV':
             image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-            self.range = [0, 180, 0, 256, 0, 256]
+            range = [0, 180, 0, 256, 0, 256]
         else:
-            # error
-            pass
+            raise ValueError("Unsupported color space: {}".format(self.color_space))
 
         # compute histogram
-        hist = cv2.calcHist([image], [0, 1, 2], None, self.histogram_bins, self.range)
+        hist = cv2.calcHist([image], [0, 1, 2], None, self.histogram_bins, range)
         hist = cv2.normalize(hist, hist).flatten()
 
         return hist
+    
 
     def save_histogram(self, hist, output_path):
-        plt.bar(range(len(hist)), hist, color='b', edgecolor='black')
+        plt.figure()
+        plt.bar(np.arange(len(hist)), hist, color='b', edgecolor='black')   # TODO: change the colors of the bars for a better visualization
+        plt.title(f"Histogram ({self.color_space} space)")
+        plt.xlabel("Bin")
+        plt.ylabel("Frequency")
+        
         plt.savefig(output_path)
+        plt.close() 
         
--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
-
+import cv2
+from descriptors import ImageDescriptor
 
 if __name__ == '__main__':
-    Image
+    cielab = ImageDescriptor('CIELAB')
+    hsv = ImageDescriptor('HSV')
+
+    # Load image and compute histograms
+    image = cv2.imread('./data/BBDD/bbdd_00000.jpg')
+    hist = cielab.describe(image)
+    cielab.save_histogram(hist, 'results/cielab_hist.png')
+
+
