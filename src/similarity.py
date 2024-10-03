3b1ec19c48f2d5dfed0c25629f88408932eb0d2e
Miren <mirensa2002@gmail.com>
2024-10-03 19:55:34 +0200

    Changed format of code in similarity

--- src/similarity.py
+++ src/similarity.py
@@ -2,7 +2,6 @@
 import cv2
 import os
 from src.descriptors import HistogramDescriptor
-import math
 import numpy as np
 from utils.preprocessing import normalizeData
 
@@ -34,34 +33,21 @@ class ComputeSimilarity:
         similarities_sorted = sorted(similarities, key=lambda x: x[1], reverse=True)
 
         return similarities_sorted
+    
+    # Bhattacharyya distance implementation (1D)
+    def bhattacharyyaDistanceGrayScale(h1, h2):
+        return cv2.compareHist(h1, h2, cv2.HISTCMP_BHATTACHARYYA)
 
+    # Bhattacharyya distance implementation (3D)
+    def bhattacharyyaDistance(h1, h2):
 
-# Bhattacharyya distance implementation (1D)
-def bhattacharyyaDistanceGrayScale(h1, h2):
-    return cv2.compareHist(h1, h2, cv2.HISTCMP_BHATTACHARYYA)
+        similarity_values = [
+            ComputeSimilarity.bhattacharyyaDistanceGrayScale(
+                normalizeData(c1,0,255), 
+                normalizeData(c2,0,255)) for c1, c2 in zip(h1, h2)]
+        tot_similarity = sum(similarity_values) / len(similarity_values)
 
-# Bhattacharyya distance implementation (3D)
-def bhattacharyyaDistance(h1, h2):
+        return tot_similarity
 
-    similarity_values = [bhattacharyyaDistanceGrayScale(normalizeData(c1,1,4), normalizeData(c2,1,4)) for c1, c2 in zip(h1, h2)]
-    tot_similarity = sum(similarity_values) / len(similarity_values)
-
-    return tot_similarity
-
-def emdDistance(h1, h2):
-    #TODO: Implement distance
-    return 0
-
-
-def testSimilarity():
-    h1 = [[1,2,3,4], [2,1,4,3], [4,3,2,1]]
-    h2 = [[2,1,4,3], [1,2,3,4], [4,3,2,1]]
-
-    print(f"Similarity between test 1 histograms: {bhattacharyyaDistance(h1, h2)}")
-
-    h3 = [[1,2,3,4], [2,1,4,3], [4,3,2,1]]
-    h4 = [[1,2,3,4], [2,1,4,3], [4,3,2,1]]
-
-    print(f"Similarity between test 2 histograms: {bhattacharyyaDistance(h3, h4)}")
 
 
