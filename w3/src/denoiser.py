b1818fb8a774297f5ced1085ae08f666faa62ef0
Miren <mirensa2002@gmail.com>
2024-10-21 21:56:40 +0200

    Started evaluating linear methods

--- w3/src/denoiser.py
+++ w3/src/denoiser.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 import pywt
+import os
+from math import log10, sqrt
+from skimage.metrics import structural_similarity as ssim
 
 class LinearDenoiser():
     def __init__(self, image):
@@ -101,7 +104,19 @@ class LinearDenoiser():
         return img_back
     
     def gaussianLowPassFilter(self, sigma):
-        return True
+        
+        if len(self.img.shape) == 3:
+
+            b, g, r = cv2.split(self.img)
+
+            b_filtered = self.gaussianLowPassGray(b, sigma)
+            g_filtered = self.gaussianLowPassGray(g, sigma)
+            r_filtered = self.gaussianLowPassGray(r, sigma)
+
+            filtered_image = cv2.merge((b_filtered, g_filtered, r_filtered))
+            return filtered_image
+        else:
+            return self.gaussianLowPassGray(self.img, sigma)
     
     def gaussianLowPassGray(self, image, sigma):
 
@@ -121,26 +136,130 @@ class LinearDenoiser():
         img_back = np.abs(img_back)
         return img_back
     
-    def waveletTransformFilter(self):
+    def waveletTransformGray(self, img, wavelet):
+        coeffs = pywt.wavedec2(img, wavelet)
+        coeffs_thresh = [coeffs[0]]  # Approximation coefficients
 
-        
+        # Apply thresholding to detail coefficients
+        for detail_level in coeffs[1:]:
+            cH, cV, cD = detail_level
+            cH = pywt.threshold(cH, np.std(cH) / 2, mode='soft')
+            cV = pywt.threshold(cV, np.std(cV) / 2, mode='soft')
+            cD = pywt.threshold(cD, np.std(cD) / 2, mode='soft')
+            coeffs_thresh.append((cH, cV, cD))
 
-        return True
-    
-    def waveletTransformGray(self, image):
+        # Reconstruct the image
+        denoised_image = pywt.waverec2(coeffs_thresh, wavelet)
+        return denoised_image
 
-        coeffs = pywt.wavedec2(image, 'haar', level = 2)
-        coeffs_thresh = list(map(lambda x: pywt.threshold(x, 10, mode='soft'), coeffs))
-        denoised_image = pywt.waverec2(coeffs_thresh, 'haar')
-        denoised_image = np.clip(denoised_image, 0, 255)
-
-        return denoised_image.astype(np.uint8)
-
+    def waveletTransformFilter(self, wavelet_type):
+        b, g, r = cv2.split(self.img)
+        b_filtered = self.waveletTransformGray(b, wavelet_type)
+        g_filtered = self.waveletTransformGray(g, wavelet_type)
+        r_filtered = self.waveletTransformGray(r, wavelet_type)
+        return cv2.merge((b_filtered, g_filtered, r_filtered))
 class NonLinearDenoiser():
     def __init__(self, image):
         self.img = image
+
+class NoiseMetric():
+    def psnr(self, orig_img, denoised_img):
+        mse = np.mean((denoised_img-orig_img)**2)
+        if mse == 0:
+            return float(200)
+        
+        max_pixel = 255.0
+        psnr_value = 20 * log10(max_pixel / sqrt(mse))
+        return psnr_value
+
+    def ssim(self, orig_img, denoised_img):
+        if len(orig_img.shape) == 3:  # If the image is colored (3 channels)
+            ssim_value = ssim(orig_img, denoised_img, win_size=3, multichannel=True)
+        else:  # For grayscale images
+            ssim_value = ssim(orig_img, denoised_img, win_size=3)
+        return ssim_value
     
+if __name__ == "__main__":
 
     
+    metrics = NoiseMetric()
+
+    linear_psnr_box = []
+    linear_psnr_median = []
+    linear_psnr_butterworth = []
+    linear_psnr_gaussian = []
+    linear_psnr_bilateral = []
+    linear_psnr_conv = []
+    linear_psnr_fft = []
+    linear_psnr_gauslow = []
+    linear_psnr_wavhaar = []
+    linear_psnr_wavdau =  []
+    linear_psnr_wavdio = []
+
+    for image_path in os.listdir('./data/qsd1_w3'):
+        if image_path.endswith('.jpg'):
+            img = cv2.imread('./data/qsd1_w3/'+image_path)
+            img_orig = cv2.imread('./data/non_augmented/'+image_path)
+
+
+            linear_denoiser = LinearDenoiser(img)
+
+            box_filter_denoised = linear_denoiser.boxFilter(3)
+            median_filter_denoised = linear_denoiser.medianFilter(3)
+            butterworth_denoised = linear_denoiser.butterworthLowPassFilter(30, 2)
+            gaussian_filter_denoised = linear_denoiser.gaussianFilter(3, 2)
+            bilateral_filter_denoised = linear_denoiser.bilateralFilter(9, 75, 75)
+            convolution_2d_denoised = linear_denoiser.Convolution2DFilter(3)
+            fft_denoised = linear_denoiser.fftLowPassFilter(30)
+            gaussian_low_denoised = linear_denoiser.gaussianLowPassFilter(30)
+            wavelet_haar_denoised = linear_denoiser.waveletTransformFilter('haar')
+            wavelet_dau_denoised = linear_denoiser.waveletTransformFilter('db1')
+            wavelet_bio_denoised = linear_denoiser.waveletTransformFilter('bior1.3')
+
+            box_filter_denoised = cv2.resize(box_filter_denoised, (img_orig.shape[1], img_orig.shape[0]))
+
+
+
+            median_filter_denoised = cv2.resize(median_filter_denoised, (img_orig.shape[1], img_orig.shape[0]))
+            butterworth_denoised = cv2.resize(butterworth_denoised, (img_orig.shape[1], img_orig.shape[0]))
+            gaussian_filter_denoised = cv2.resize(gaussian_filter_denoised, (img_orig.shape[1], img_orig.shape[0]))
+            bilateral_filter_denoised = cv2.resize(bilateral_filter_denoised, (img_orig.shape[1], img_orig.shape[0]))
+            convolution_2d_denoised = cv2.resize(convolution_2d_denoised, (img_orig.shape[1], img_orig.shape[0]))
+            fft_denoised = cv2.resize(fft_denoised, (img_orig.shape[1], img_orig.shape[0]))
+            gaussian_low_denoised = cv2.resize(gaussian_low_denoised, (img_orig.shape[1], img_orig.shape[0]))
+            wavelet_haar_denoised = cv2.resize(wavelet_haar_denoised, (img_orig.shape[1], img_orig.shape[0]))
+            wavelet_dau_denoised = cv2.resize(wavelet_dau_denoised, (img_orig.shape[1], img_orig.shape[0]))
+            wavelet_bio_denoised = cv2.resize(wavelet_bio_denoised, (img_orig.shape[1], img_orig.shape[0]))
+
+
+            linear_psnr_box.append(metrics.psnr(img_orig, box_filter_denoised))
+            linear_psnr_median.append(metrics.psnr(img_orig, median_filter_denoised))
+            linear_psnr_butterworth.append(metrics.psnr(img_orig, butterworth_denoised))
+            linear_psnr_gaussian.append(metrics.psnr(img_orig, gaussian_filter_denoised))
+            linear_psnr_bilateral.append(metrics.psnr(img_orig, bilateral_filter_denoised))
+            linear_psnr_conv.append(metrics.psnr(img_orig, convolution_2d_denoised))
+            linear_psnr_fft.append(metrics.psnr(img_orig, fft_denoised))
+            linear_psnr_gauslow.append(metrics.psnr(img_orig, gaussian_low_denoised))
+            linear_psnr_wavhaar.append(metrics.psnr(img_orig, wavelet_haar_denoised))
+            linear_psnr_wavdau.append(metrics.psnr(img_orig, wavelet_dau_denoised))
+            linear_psnr_wavdio.append(metrics.psnr(img_orig, wavelet_bio_denoised))
+
+
+
+    
+    print("----------------------------------------------------------------------------------------------------------------------\n")
+    print("LINEAR DENOISING METHODS: EVALUATION \n")
+
+    print("Box Filter --- PSNR: " + str(np.mean(linear_psnr_box)) + "\n")
+    print("Median filter --- PSNR: "+ str(np.mean(linear_psnr_median))+"\n")
+    print("Butterworth filter --- PSNR: "+str(np.mean(linear_psnr_butterworth))+"\n")
+    print("Gaussian filter --- PSNR: "+str(np.mean(linear_psnr_gaussian))+"\n")
+    print("Bilateral filter --- PSNR: "+str(np.mean(linear_psnr_bilateral))+"\n")
+    print("Convolutional filter --- PSNR: "+str(np.mean(linear_psnr_conv))+"\n")
+    print("Fast Fourier Transform --- PSNR: "+str(np.mean(linear_psnr_fft))+"\n")
+    print("Gaussian Low Pass filter --- PSNR: "+str(np.mean(linear_psnr_gauslow))+"\n")
+    print("Wavelet Transform Haar --- PSNR: "+str(np.mean(linear_psnr_wavhaar))+"\n")
+    print("Wavelet Transform Daubechies --- PSNR: "+str(np.mean(linear_psnr_wavdau))+"\n")
+    print("Wavelet Transform BiOrthogonal --- PSNR: "+str(np.mean(linear_psnr_wavdio))+"\n")
 
 
