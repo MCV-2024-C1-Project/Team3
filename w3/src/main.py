f27ced059099fe39e401e84d7be8da3d4b2e0394
Laura Haro Escoi <lauraha.escoi@gmail.com>
2024-10-26 13:30:16 +0200

    Fixed what merge broke :D

--- w3/src/main.py
+++ w3/src/main.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 import pickle
 import argparse
-from descriptors import ImageDescriptor
+from descriptors import ImageDescriptor, TextureDescriptor
 from similarity import ComputeSimilarity
 from evaluation.average_precision import mapk
 from background_removal import CalculateBackground
@@ -15,7 +15,7 @@ from denoiser import denoiseAll, LinearDenoiser
 # Argument parser setup
 parser = argparse.ArgumentParser(description='Process image folder for similarity.')
 parser.add_argument('images_folder', type=str, help='Path to the image folder (e.g., ./data/qsd1_w1)')
-parser.add_argument('dimension', type=str, help='Histogram dimension (e.g., 3D')
+parser.add_argument('structure', type=str, help='Structure of the descriptor (e.g., block, DCT, LBP, heriarchical)')
 parser.add_argument('colorspace', type=str, help='Histogram colorspace (e.g., HSV')
 # parser.add_argument('quantization', type=bool, help='Quantize dct descriptor')
 parser.add_argument('measure', type=str, help='Similarity measure (e.g., intersection')
@@ -24,7 +24,7 @@ args = parser.parse_args()
 
 # Define input folder based on argument
 qsd_folder = args.images_folder
-dimension = args.dimension
+structure = args.structure
 colorspace = args.colorspace
 quantization = False
 measure = args.measure
@@ -39,7 +39,7 @@ QST2_W2_FOLDER = os.path.join(DATA_FOLDER, 'qst2_w1')
 RESULTS_FOLDER = './results'
 GT_CORRESPS_FILE = os.path.join(qsd_folder, 'gt_corresps.pkl')
 
-MASK_FOLDER = './masks'
+MASK_FOLDER = './data/masks'
 NO_BG_FOLDER = './data/qsd2_w3_no_bg'
 
 METHOD1_FOLDER = os.path.join(RESULTS_FOLDER, 'method1')  # Output folder for method1
@@ -47,20 +47,21 @@ METHOD2_FOLDER = os.path.join(RESULTS_FOLDER, 'method2')  # Output folder for me
 
 FINAL_IMAGES = './data/final_images/'
 
-def load_histograms(dimension, structure, descriptor, folder_path):
+def load_histograms(structure, descriptor, folder_path,quantization):
     """Load histograms from file if available, otherwise calculate them."""
-    histogram_path = '_'.join(('results/histograms', dimension, descriptor.color_space, structure)) + '.npy'
+    histogram_path = '_'.join(('results/descriptors', descriptor.color_space, structure,str(quantization))) + '.npy'
     if os.path.exists(histogram_path):
         print(f"Loading histograms from {histogram_path}...")
         histograms = np.load(histogram_path, allow_pickle=True).item()
     else:
-        print(f"Calculating histograms for {dimension}, {structure}, {descriptor.color_space}...")
-        histograms = process_images(folder_path, dimension, structure, descriptor)
+        print(f"Calculating histograms for {structure}, {descriptor.color_space}, quantization {quantization}...")
+        histograms = process_images(folder_path, structure, descriptor,quantization)
         np.save(histogram_path, histograms)
         print(f"Histograms saved to {histogram_path}")
     return histograms
 
-def process_images(folder_path, dimension, structure, descriptor):
+
+def process_images(folder_path, structure, descriptor,quantization):
     """Calculate histograms for all images in the folder."""
     histograms_dict = {}
     for filename in sorted(os.listdir(folder_path)):
@@ -70,15 +71,16 @@ def process_images(folder_path, dimension, structure, descriptor):
             if image is None:
                 print(f"Error loading image {image_path}")
                 continue
-            hist = descriptor.describe(image, dimension, structure)
+            hist = descriptor.describe(image, structure,quantization)
             histograms_dict[filename] = {
                 'color_space': descriptor.color_space,
-                'dimension': dimension,
+                'quantization': quantization,
                 'structure': structure,
-                'histogram_bins': descriptor.histogram_bins,
-                'histograms': hist
+                'descriptor': hist
             }
     return histograms_dict
+
+
 def kullback_leibler_divergence(p, q):
     p = np.asarray(p)
     q = np.asarray(q)
@@ -107,17 +109,17 @@ def detect_pictures(image, mask):
     
     return cuadros
 
-def calculate_similarity(histograms, descriptor, labels, K, similarity_measure, dimension, structure, level, mask, folder=qsd_folder):
+def calculate_similarity(histograms, descriptor, labels, K, similarity_measure, quantization, structure, level, mask, folder):
     """Calculate mAP@K for a given similarity measure and descriptor."""
     measures = ComputeSimilarity()
     top_K = []
 
     for img in sorted(os.listdir(folder)):
         image_path = os.path.join(folder, img)
-        mask_path = os.path.join(MASK_FOLDER, img)
+        mask_path = os.path.join(MASK_FOLDER, img.split('.')[0] + ".png")
 
         # Check if the file is an image
-        if not img.endswith(".jpg") :
+        if not img.endswith(".jpg"):
             continue
         
         try:
@@ -128,45 +130,57 @@ def calculate_similarity(histograms, descriptor, labels, K, similarity_measure, 
             print(f"Error processing image {img}: {e}")
             continue
         
-        if structure == 'DCT' or structure == 'LBP' or structure == 'DCT_simple':
-            if similarity_measure == "intersection":
-                similarities = {key: measures.histogramIntersection(np.array(histogram,dtype=np.float32).flatten(), 
-                                                                    np.array(value['descriptor'],dtype=np.float32).flatten()) for key, value in histograms.items()}
-                reverse = True
-            elif similarity_measure == "bhattacharyya":
-                similarities = {key: measures.bhattacharyyaDistance(np.array(histogram,dtype=np.float32).flatten(), 
-                                                                    np.array(value['descriptor'],dtype=np.float32).flatten()) for key, value in histograms.items()}
-                reverse = True
-            elif similarity_measure == "Chisqr":
-                similarities = {key: measures.histogramChisqr(np.array(histogram,dtype=np.float32).flatten(), 
-                                                                    np.array(value['descriptor'],dtype=np.float32).flatten()) for key, value in histograms.items()}
-                reverse = True
-            elif similarity_measure == "Correl":
-                similarities = {key: measures.histogramCorrel(np.array(histogram,dtype=np.float32).flatten(), 
-                                                                    np.array(value['descriptor'],dtype=np.float32).flatten()) for key, value in histograms.items()}
-                reverse = False
-            elif similarity_measure == "kullback":
-                similarities = {key: kullback_leibler_divergence(np.array(histogram,dtype=np.float32).flatten(), 
-                                                                    np.array(value['descriptor'],dtype=np.float32).flatten()) for key, value in histograms.items()}
-                reverse = False
-            elif similarity_measure=="euclidean":
-                similarities = {key: sum(pow(abs(np.array(np.array(value['descriptor'],dtype=np.float32).flatten()-np.array(histogram,dtype=np.float32).flatten(),dtype=np.float32).flatten()),2)) for key, value in histograms.items()}
-                reverse = False
-            elif similarity_measure=="hellinger":
-                similarities = {key: np.sum(np.sqrt(np.multiply(np.array(value['descriptor'],dtype=np.float32).flatten(),np.array(histogram,dtype=np.float32).flatten()))) for key, value in histograms.items()}
-                
-                reverse = True
+        img_top_K = []
+
+        for pict in pictures:
+            try:
+                histogram = descriptor.describe(pict, structure, quantization)
+            except Exception as e:
+                print(f"Error processing image {img}: {e}")
+                continue
+
+            if structure == 'DCT' or structure == 'LBP' or structure == 'DCT_simple':
+                if similarity_measure == "intersection":
+                    similarities = {key: measures.histogramIntersection(np.array(histogram,dtype=np.float32).flatten(), 
+                                                                        np.array(value['descriptor'],dtype=np.float32).flatten()) for key, value in histograms.items()}
+                    reverse = True
+                elif similarity_measure == "bhattacharyya":
+                    similarities = {key: measures.bhattacharyyaDistance(np.array(histogram,dtype=np.float32).flatten(), 
+                                                                        np.array(value['descriptor'],dtype=np.float32).flatten()) for key, value in histograms.items()}
+                    reverse = True
+                elif similarity_measure == "Chisqr":
+                    similarities = {key: measures.histogramChisqr(np.array(histogram,dtype=np.float32).flatten(), 
+                                                                        np.array(value['descriptor'],dtype=np.float32).flatten()) for key, value in histograms.items()}
+                    reverse = True
+                elif similarity_measure == "Correl":
+                    similarities = {key: measures.histogramCorrel(np.array(histogram,dtype=np.float32).flatten(), 
+                                                                        np.array(value['descriptor'],dtype=np.float32).flatten()) for key, value in histograms.items()}
+                    reverse = False
+                elif similarity_measure == "kullback":
+                    similarities = {key: kullback_leibler_divergence(np.array(histogram,dtype=np.float32).flatten(), 
+                                                                        np.array(value['descriptor'],dtype=np.float32).flatten()) for key, value in histograms.items()}
+                    reverse = False
+                elif similarity_measure=="euclidean":
+                    similarities = {key: sum(pow(abs(np.array(np.array(value['descriptor'],dtype=np.float32).flatten()-np.array(histogram,dtype=np.float32).flatten(),dtype=np.float32).flatten()),2)) for key, value in histograms.items()}
+                    reverse = False
+                elif similarity_measure=="hellinger":
+                    similarities = {key: np.sum(np.sqrt(np.multiply(np.array(value['descriptor'],dtype=np.float32).flatten(),np.array(histogram,dtype=np.float32).flatten()))) for key, value in histograms.items()}
+                    
+                    reverse = True
 
 
 
-            top_k = [k for k, v in sorted(similarities.items(), key=lambda item: item[1], reverse=reverse)][:K]
-            top_k_numbers = [int(filename.split('.')[0].split('_')[-1]) for filename in top_k]
-            top_K.append(top_k_numbers)
+                top_k = [k for k, v in sorted(similarities.items(), key=lambda item: item[1], reverse=reverse)][:K]
+                top_k_numbers = [int(filename.split('.')[0].split('_')[-1]) for filename in top_k]
+                img_top_K.append(top_k_numbers)
+                print(f"Top {K} images for {img}: {top_k_numbers}")
+        
+        top_K.append(img_top_K)
 
     return top_K  # Return top K results (list of lists)
 
 
-def process_similarity_measures(histograms, descriptor, labels, dimension, structure, k_val, method_folder, measure, mask, images_folder=qsd_folder):
+def process_similarity_measures(histograms, descriptor, labels, quantization, structure, k_val, method_folder, measure, mask, images_folder=qsd_folder):
     """Process all combinations of similarity measures for a single descriptor."""
     # similarity_measures = ["intersection", "canberra"]
 
@@ -174,14 +188,14 @@ def process_similarity_measures(histograms, descriptor, labels, dimension, struc
     if structure == 'DCT' or structure =="LBP" or structure == 'DCT_simple':
         top_K = calculate_similarity(histograms, descriptor, labels, k_val, measure, quantization, structure, None, mask=mask, folder=images_folder)
         map_k = mapk(labels, top_K, k_val)
-        print(f"mAP@{k_val} for {dimension}, {structure}, {descriptor.color_space} and {measure}: {map_k}")
+        print(f"mAP@{k_val} for quantization {quantization}, {structure}, {descriptor.color_space} and {measure}: {map_k}")
     
-    elif structure == 'block' or structure == 'heriarchical':
-        for level in range(3):
-            top_K = calculate_similarity(histograms, descriptor, labels, k_val, measure, dimension, structure, level,mask=mask, folder=images_folder)
-            map_k = mapk(labels, top_K, k_val)
-            # Calculate mAP
-            print(f"mAP@{k_val} for {dimension}, {structure}, {descriptor.color_space} at level {level} and {measure}: {map_k}")
+    # elif structure == 'block' or structure == 'heriarchical':
+    #     for level in range(3):
+    #         top_K = calculate_similarity(histograms, descriptor, labels, k_val, measure, quantization, structure, level,mask=mask, folder=images_folder)
+    #         map_k = mapk(labels, top_K, k_val)
+    #         # Calculate mAP
+    #         print(f"mAP@{k_val} for quantization {quantization}, {structure}, {descriptor.color_space} at level {level} and {measure}: {map_k}")
 
     if not os.path.exists(method_folder):
         os.makedirs(method_folder)
@@ -229,7 +243,7 @@ def compute_precision_recall_f1(ground_truth, predicted):
 def background_images(qsd_folder):
     # Calculate background images
     final_image=None
-    if qsd_folder == "./data/qsd2_w3" or qsd_folder == "./data\qst2_w1" or qsd_folder == "./data/qst2_w1":
+    if qsd_folder == "./data/qsd2_w3" or qsd_folder == "./data\qst2_w1" or qsd_folder == "./data/qst2_w1" or qsd_folder == FINAL_IMAGES:
         print("Removing background")
         NO_BG_FOLDER = './data/qsd2_w3_no_bg'
         if not os.path.exists(NO_BG_FOLDER):
@@ -278,7 +292,7 @@ def background_images(qsd_folder):
 
                 cv2.imwrite(os.path.join(MASK_FOLDER, image_name.split('.')[0] + ".png"), final_image)
 
-                if qsd_folder == "./data/qsd2_w3":
+                if qsd_folder == "./data/qsd2_w3" or qsd_folder == FINAL_IMAGES:
                     gt = cv2.imread(os.path.join("./data/qsd2_w3/", image_name[:-4] + ".png"), cv2.IMREAD_GRAYSCALE)
                     if gt is not None:
                         intersection = np.logical_and(gt, final_image)
@@ -337,7 +351,7 @@ if __name__ == '__main__':
 
     _, mask = background_images(FINAL_IMAGES)
     # After all masks have been applied and evaluated, process similarity with the background-removed images
-    print("Processing similarity using method:", colorspace, "dimension:", dimension, "structure:", structure)
+    print("Processing similarity using method:", colorspace, "structure:", structure)
     print(qsd_folder)
     process_similarity_measures(histograms, TextureDescriptor(colorspace), labels, quantization, structure, k_val=1, mask=cv2.bitwise_not(mask), measure=measure, method_folder=METHOD1_FOLDER, images_folder=FINAL_IMAGES)
                 
